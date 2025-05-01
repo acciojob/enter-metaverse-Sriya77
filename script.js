291591cf915404d5a544b3cb9b226e9f89c05f7f
SRIYA CHAKINALA <142564481+Sriya77@users.noreply.github.com>
2025-05-01 18:18:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const btn = document.getElementById("enterBtn");
+const statusPara = document.getElementById("status");
+
+btn.addEventListener("click", () => {
+  const h1 = document.createElement("h1");
+  h1.innerText = "Entered Metaverse";
+  statusPara.replaceWith(h1); // Replace the <p> with the new <h1>
+});
